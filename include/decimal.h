c51a5183afd21aa2603af1b8505291cb760df470
vpiotr <vpiotr@poczta.onet.pl>
2014-02-01 16:43:45 +0100

    More precise string to decimal conversion (without using double). Also more documentation for functions.

--- include/decimal.h
+++ include/decimal.h
@@ -4,8 +4,8 @@
 //              operations on currency values.
 // Author:      Piotr Likus
 // Created:     03/01/2011
-// Last change: 29/07/2012
-// Version:     1.4
+// Last change: 01/02/2014
+// Version:     1.5
 // Licence:     BSD
 /////////////////////////////////////////////////////////////////////////////
 
@@ -28,7 +28,7 @@
 
 #include <iosfwd>
 #include <iomanip>
-#include <sstream> 
+#include <sstream>
 #include <locale>
 
 #ifndef DEC_EXTERNAL_LIMITS
@@ -288,9 +288,16 @@ public:
       beforeValue = (m_value - afterValue) / DecimalFactor<Prec>::value;
     }
 
-    /// combines two parts (before and after decimal point) into decimal value
+    /// Combines two parts (before and after decimal point) into decimal value
+    /// \param[in] before value before decimal point
+    /// \param[in] after value after decimal point multiplied by 10^prec
+    /// \result Returns *this
     decimal &pack(int64 beforeValue, int64 afterValue) {
-      m_value = beforeValue * DecimalFactor<Prec>::value + afterValue;
+      if (Prec > 0) {
+        m_value = beforeValue * DecimalFactor<Prec>::value;
+        m_value += (afterValue % DecimalFactor<Prec>::value);
+       } else
+        m_value = beforeValue * DecimalFactor<Prec>::value;
       return *this;
     }
 
@@ -422,32 +429,16 @@ decimal<Prec> decimal_cast(int arg)
     return decimal<Prec>(static_cast<double>(arg));
 }
 
-// input
-template <class charT, class traits, int prec>
-  std::basic_istream<charT, traits> &
-    operator>>(std::basic_istream<charT, traits> & is, decimal<prec> & d)
-{
-  double dbl;
-  is >> dbl;
-  d.setAsDouble(dbl);
-  return is;
-}
-
-// output
-template <class charT, class traits, int prec>
-  std::basic_ostream<charT, traits> &
-    operator<<(std::basic_ostream<charT, traits> & os, const decimal<prec> & d)
-{
-  std::string helper;
-  os << toString(d, helper);
-  return os;
-}
-
-template <int prec>
-std::string &toString(const decimal<prec> &arg, std::string &output) {
+/// Exports decimal to stream
+/// Used format: {-}bbbb.aaaa where
+/// {-} is optional '-' sign character
+/// '.' is locale-dependent decimal point character
+/// bbbb is stream of digits before decimal point
+/// aaaa is stream of digits after decimal point
+template <int prec, typename StreamType>
+void toStream(const decimal<prec> &arg, StreamType &output) {
   using namespace std;
 
-  ostringstream out;
   int64 before, after;
   int sign;
 
@@ -467,20 +458,198 @@ std::string &toString(const decimal<prec> &arg, std::string &output) {
   }
 
   if (sign < 0)
-    out << "-";
- 
-  char dec_point = use_facet<numpunct<char> >(out.getloc()).decimal_point();
-  out << before << dec_point;
-  out << setw(arg.getDecimalPoints()) << setfill('0') << right << after;
+    output << "-";
+
+  const char dec_point = use_facet<numpunct<char> >(output.getloc()).decimal_point();
+  output << before << dec_point;
+  output << setw(arg.getDecimalPoints()) << setfill('0') << right << after;
+}
+
+/// Converts stream of chars to decimal
+/// Handles the following formats ('.' is selected from locale info):
+/// \code
+/// 123
+/// -123
+/// 123.0
+/// -123.0
+/// 123.
+/// .123
+/// 0.
+/// -.123
+/// \endcode
+/// Spaces and tabs on the front are ignored.
+/// \param[in] input input stream
+/// \param[out] output decimal value, 0 on error
+/// \result Returns true if conversion succeeded
+template <int prec, typename StreamType>
+bool fromStream(StreamType &input, decimal<prec> &output) {
+    using namespace std;
+    int64 before, after;
+    int sign = 1;
+
+    enum StateEnum { IN_SIGN, IN_BEFORE_FIRST_DIG, IN_BEFORE_DEC, IN_AFTER_DEC, IN_END} state = IN_SIGN;
+    const char dec_point = use_facet<numpunct<char> >(input.getloc()).decimal_point();
+    enum ErrorCodes { ERR_WRONG_CHAR = -1, ERR_NO_DIGITS = -2, ERR_WRONG_STATE = -3, ERR_STREAM_GET_ERROR = -4 };
+
+    before = after = 0;
+    int error = 0;
+    int digitsCount = 0;
+    int afterDigitCount = 0;
+    char c;
+
+    while ((input) && (state != IN_END))          // loop while extraction from file is possible
+    {
+        c = static_cast<char>(input.get());
+
+        switch (state) {
+          case IN_SIGN:
+            if (c == '-') {
+                sign = -1;
+                state = IN_BEFORE_FIRST_DIG;
+            } else if (c == '+') {
+                state = IN_BEFORE_FIRST_DIG;
+            } else if ((c >= '0') && (c <= '9'))
+            {
+                state = IN_BEFORE_DEC;
+                before = static_cast<int>(c - '0');
+                digitsCount++;
+            } else if (c == dec_point) {
+                state = IN_AFTER_DEC;
+            } else if ((c != ' ') && (c != '\t')) {
+                state = IN_END;
+                error = ERR_WRONG_CHAR;
+            }
+            // else ignore char
+            break;
+         case IN_BEFORE_FIRST_DIG:
+            if ((c >= '0') && (c <= '9')) {
+                before = 10 * before + static_cast<int>(c - '0');
+                state = IN_BEFORE_DEC;
+                digitsCount++;
+            } else if (c == dec_point) {
+                state = IN_AFTER_DEC;
+            } else {
+                state = IN_END;
+                error = ERR_WRONG_CHAR;
+            }
+            break;
+         case IN_BEFORE_DEC:
+            if ((c >= '0') && (c <= '9')) {
+                before = 10 * before + static_cast<int>(c - '0');
+                digitsCount++;
+            } else if (c == dec_point) {
+                state = IN_AFTER_DEC;
+            } else {
+                state = IN_END;
+            }
+            break;
+         case IN_AFTER_DEC:
+            if ((c >= '0') && (c <= '9')) {
+                after = 10 * after + static_cast<int>(c - '0');
+                afterDigitCount++;
+                if (afterDigitCount >= prec)
+                   state = IN_END;
+            } else {
+                state = IN_END;
+                if (digitsCount == 0) {
+                    error = ERR_NO_DIGITS;
+                }
+            }
+            break;
+         default:
+            error = ERR_WRONG_STATE;
+            state = IN_END;
+            break;
+        } // switch state
+  } // while stream good & not end
+
+  if (error >= 0)
+  {
+    if (afterDigitCount > 0)
+    {
+       // convert 4 to 4000 for decimal<4>
+       int leftPow = prec - afterDigitCount;
+       while (leftPow > 0)
+       {
+           after *= 10;
+           leftPow--;
+       }
+    }
+
+    if (sign < 0) {
+        before = -before;
+        after = -after;
+    }
+
+    output.pack(before, after);
+  } else {
+    output.pack(0,0);
+  }
+
+  return (error >= 0);
+} // function
+
+/// Exports decimal to string
+/// Used format: {-}bbbb.aaaa where
+/// {-} is optional '-' sign character
+/// '.' is locale-dependent decimal point character
+/// bbbb is stream of digits before decimal point
+/// aaaa is stream of digits after decimal point
+template <int prec>
+std::string &toString(const decimal<prec> &arg, std::string &output) {
+  using namespace std;
+
+  ostringstream out;
+  toStream(arg, out);
   output = out.str();
   return output;
 }
 
+/// Exports decimal to string
+/// Used format: {-}bbbb.aaaa where
+/// {-} is optional '-' sign character
+/// '.' is locale-dependent decimal point character
+/// bbbb is stream of digits before decimal point
+/// aaaa is stream of digits after decimal point
 template <int prec>
 std::string toString(const decimal<prec> &arg) {
   std::string res;
   toString(arg, res);
   return res;
+}
+
+// input
+template <class charT, class traits, int prec>
+  std::basic_istream<charT, traits> &
+    operator>>(std::basic_istream<charT, traits> & is, decimal<prec> & d)
+{
+  if (!fromStream<prec>(is, d))
+    d.setUnbiased(0);
+  return is;
+}
+
+// output
+template <class charT, class traits, int prec>
+  std::basic_ostream<charT, traits> &
+    operator<<(std::basic_ostream<charT, traits> & os, const decimal<prec> & d)
+{
+  std::string helper;
+  os << toString(d, helper);
+  return os;
+}
+
+/// Imports decimal from string
+/// Used format: {-}bbbb.aaaa where
+/// {-} is optional '-' sign character
+/// '.' is locale-dependent decimal point character
+/// bbbb is stream of digits before decimal point
+/// aaaa is stream of digits after decimal point
+template <typename T>
+T fromString (const std::string &str) {
+    std::istringstream is(str);
+    T t;
+    is >> t;
+    return t;
 }
 
 } // namespace
