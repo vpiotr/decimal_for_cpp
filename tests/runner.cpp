21bf1bc78ab00a728139f66895f1704fc35dcb2c
vpiotr <vpiotr@poczta.onet.pl>
2015-01-25 10:48:45 +0100

    Boost.Test (1.57) under MinGW is broken and needs a small workaround.

--- tests/runner.cpp
+++ tests/runner.cpp
@@ -1,13 +1,18 @@
 /////////////////////////////////////////////////////////////////////////////
 // Name:        runner.cpp
 // Project:     decimal
-// Purpose:     Test runner for decimal library 
+// Purpose:     Test runner for decimal library
 //              Include it as the only .CPP file.
 // Author:      Piotr Likus
 // Modified by:
 // Created:     01/02/2014
 // Licence:     BSD
 /////////////////////////////////////////////////////////////////////////////
+
+#ifdef __MINGW32__
+// Mingw doesn't define putenv() needed by Boost.Test
+extern int putenv(char*);
+#endif
 
 #define BOOST_TEST_MAIN
 #define BOOST_TEST_MODULE "C++ Unit Tests for decimal"
