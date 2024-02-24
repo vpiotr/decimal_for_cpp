87b31e95a70020c01a0ef9da247261150b14456d
Piotr <vpiotr@poczta.onet.pl>
2024-02-24 18:01:45 +0100

    #61: code cleanup

--- tests/runner.cpp
+++ tests/runner.cpp
@@ -25,26 +25,9 @@ BOOST_AUTO_TEST_CASE(decimalBasicTest) {
     using namespace dec;
     using namespace std;
 
-    // the following declares currency variable with 2 decimal points
-    // initialized with integer value (can be also floating-point)
-    decimal<2> value(143125);
+    decimal<2> value1(143125);
+    decimal<2> value2("143125");
 
-    // displays: Value #1 is: 143125.00
-    cout << "Value #1 is: " << value << endl;
+    BOOST_CHECK_EQUAL(value1, value2);
 }
 
-//BOOST_AUTO_TEST_SUITE ( test )
-//#include "decimalTest.cpp"
-//#include "decimalTestAbout.cpp"
-//#include "decimalTestArithmetic.cpp"
-//#include "decimalTestDiv.cpp"
-//#include "decimalTestEdgeCases.cpp"
-//#include "decimalTestModulo.cpp"
-//#include "decimalTestMult.cpp"
-//#include "decimalTestMultDiv.cpp"
-//#include "decimalTestRounding.cpp"
-//#include "decimalTestRoundOth.cpp"
-//#include "decimalTestString.cpp"
-//#include "decimalTestTypeLevel.cpp"
-//#include "decimalTestWithExponent.cpp"
-//BOOST_AUTO_TEST_SUITE_END( )
